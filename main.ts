61d4221f9897f83fd1f299ad984c0bc115b27af8
siddhanthg <96607298+siddhanthg@users.noreply.github.com>
2021-12-24 10:15:55 +0530

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,7 @@
+basic.forever(function () {
+    music.playMelody("B G B G B C5 B C5 ", 150)
+    music.playMelody("A C5 A C5 A G B G ", 150)
+    music.playMelody("E - D F A - C - ", 150)
+    music.playMelody("A E A E A C5 A C5 ", 150)
+    music.playMelody("E - D F A - C - ", 150)
+})
